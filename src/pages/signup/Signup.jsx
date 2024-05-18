a7c817cd4cd5b19eef45819a0b5ed743d4ad38f3
minset3489 <minn.set@gmail.com>
2024-05-18 14:45:30 +0700

    Creadted handleFileChange and handleSubmit functions

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -5,12 +5,38 @@ const Signup = () => {
     const[email, setEmail] = useState('')
     const[password, setPassword] = useState('')
     const[displayName, setDisplayName] = useState('')
-    const[thumbnail, setThumbnail] = useState('')
+    const[thumbnail, setThumbnail] = useState(null)
 
+    //allowed file types
+    const types = ['image/png', 'image/jpeg']
+    const [fileError, setFileError] = useState(null)
+
+    const handleFileChange = (e) => {
+        setThumbnail(null);
+        let selected = e.target.files[0];
+        console.log(selected);
+        if (selected && types.includes(selected.type)) {
+            if(selected.size > 200000){
+                setFileError('Image file size must be less than 200kb');
+                return
+            }
+            setThumbnail(selected);
+            setFileError(null);
+            console.log('thumbnail updated');
+        } else {
+            setThumbnail(null);
+            setFileError('Please select an image file (png or jpeg)');
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email,password,displayName,thumbnail)
+    }
 
     return ( <>
-    <form className="auth-form">
-        <h2>Sign up</h2>
+    <form className=" max-w-[400px] my-16 mx-auto p-10 border border-gray-300 shadow-md bg-white" onSubmit={handleSubmit}>
+        <h2 className=" font-bold">Sign up</h2>
         <label>
             <span>Email :</span>
             <input 
@@ -43,7 +69,11 @@ const Signup = () => {
             <input 
                 required
                 type="file"
+                onChange={handleFileChange}
             />
+            {fileError && <div className="error">
+                {fileError}
+            </div>}
         </label>
         <button className="btn">Sign up</button>
     </form>
