a24ad9fbbe2c704b1ad3cfaa58ad8ef16d63f1fc
minset3489 <minn.set@gmail.com>
2024-05-18 13:54:18 +0700

    Created Form

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,6 +1,52 @@
+import { useState } from "react";
+
 const Signup = () => {
+
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+    const[displayName, setDisplayName] = useState('')
+    const[thumbnail, setThumbnail] = useState('')
+
+
     return ( <>
-    <div>Signup</div>
+    <form className="auth-form">
+        <h2>Sign up</h2>
+        <label>
+            <span>Email :</span>
+            <input 
+                required
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+            />
+        </label>
+        <label>
+            <span>Password :</span>
+            <input 
+                required
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+            />
+        </label>
+        <label>
+            <span>Display Name :</span>
+            <input 
+                required
+                type="text"
+                onChange={(e) => setDisplayName(e.target.value)}
+                value={displayName}
+            />
+        </label>
+        <label>
+            <span>Profile Thumbnail :</span>
+            <input 
+                required
+                type="file"
+            />
+        </label>
+        <button className="btn">Sign up</button>
+    </form>
     </> );
 }
  
