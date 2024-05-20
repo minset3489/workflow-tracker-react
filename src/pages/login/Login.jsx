ed0f99acf7ead98aa5db641e3d69036f02bda979
minset3489 <minn.set@gmail.com>
2024-05-20 12:58:20 +0700

    Created Signup Form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,46 @@
+import { useState } from "react";
+
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password)
+    };
+
+    
+
     return ( <>
-    <div>Login</div>
+    <form className="max-w-[400px] my-16 mx-auto p-10 border border-gray-300 shadow-md bg-white" onSubmit={handleSubmit}>
+      <h2 className="font-bold">Sign up</h2>
+      <label>
+        <span>Email :</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password :</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+     
+      {!isPending && <button className="btn">Sign up</button>}
+      {isPending && <button className="btn" disabled>Loading</button>}
+      {error && <div className="error">{error}</div>}
+    </form>
     </> );
 }
  
