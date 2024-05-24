3d672ca5b18862cb776fd65f700cd6a97fd7b132
minset3489 <minn.set@gmail.com>
2024-05-24 13:45:05 +0700

    updated firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,15 +2,6 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBgOfrRemQoBobncbjsnMM5Ukzioq14Hag",
-//     authDomain: "workflow-tracker-26d81.firebaseapp.com",
-//     projectId: "workflow-tracker-26d81",
-//     storageBucket: "workflow-tracker-26d81.appspot.com",
-//     messagingSenderId: "263451888287",
-//     appId: "1:263451888287:web:9587a0db172acf544670aa"
-//   };
-
 const firebaseConfig = {
   apiKey: "AIzaSyAHcnLbeH_nJrFMjX6XcyTIm1aJdHL-wQ4",
   authDomain: "workflow-tracker-dba59.firebaseapp.com",
